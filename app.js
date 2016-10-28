75addd2c084e925e5c38a0fda29e7a300f39dad2
ybinstock <ybinstock@gmail.com>
2016-10-28 14:05:55 -0400

    fixed audio

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
 $(document).ready(function() {
-
   var script = document.querySelector('#script');
-  var wind2 = document.querySelector('#wind2');
-  var plane = document.querySelector('#plane');
-  var splash = document.querySelector('#splash');
-
-//for wind
-
-
 });
